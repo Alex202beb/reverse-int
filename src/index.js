90fea87b92502fa9d39321bc7ec9bcce094bd0de
Alex202beb <holiwaa@yandex.ru>
2023-01-15 02:16:42 +0200

    feat: add revers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let j = String(n);
+    let f = [...j].reverse().join("");
+	if (f.includes("-") == true){
+		return f.slice(0, -1); 
+	} else {
+		return f;
+	}
 }
